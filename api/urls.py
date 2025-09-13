28b03bee77d5273f4dc511470204c361637c1046
Syed Asadullah Zeeshan .S.M <146610208+syedzeeeshan@users.noreply.github.com>
2025-09-13 07:19:02 -0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('health/', views.health_check, name='health_check'),
-    path('people/', views.people_list, name='people_list'),  # For your addPerson function
+    path('people/', views.people_list, name='people_list'),  
 ]
